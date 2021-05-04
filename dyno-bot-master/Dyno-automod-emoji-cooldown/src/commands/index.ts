59b9bc49729e626c9d412d772be46f7260a8a111
NightRaven <53287573+Xynnix@users.noreply.github.com>
2021-05-04 01:54:01 -0500

    Delete dyno-bot-master/Dyno-automod-emoji-cooldown directory

--- dyno-bot-master/Dyno-automod-emoji-cooldown/src/commands/index.ts
+++
@@ -1,3 +0,0 @@
-// export {default as Automod} from './Automod';
-// export {default as Blacklist} from './Blacklist';
-// export {default as Whitelist} from './Whitelist';
--- dyno-bot-master/Dyno-automod-emoji-cooldown/src/index.ts
+++
@@ -1,5 +0,0 @@
-require('source-map-support').install();
-
-export {default as Automod} from './Automod';
-export {default as Logger} from './Logger';
-export {default as Moderator} from './Moderator';
